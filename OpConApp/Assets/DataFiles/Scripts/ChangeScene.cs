747ec98b314e95da8eed79812b91b3b854913c2a
Th3Yukio <67382757+Th3Yukio@users.noreply.github.com>
2020-06-24 13:01:15 -0300

    GitStart

---
+++ OpConApp/Assets/DataFiles/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void analyzer()
+    {
+        SceneManager.LoadScene("Analyzer");
+    }
+    
+    public void menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+   public void closeapp()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button pressed");
+    }
+}
